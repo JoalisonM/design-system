a1aa948d858db0db895058aa8f91e198d408c92a
Joalison Ferreira <joalison.ferreira@nefex.io>
2025-02-17 10:50:54 -0300

    fix: fix storybook main

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,42 +1,46 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
-import { join, dirname } from "path";
+import { dirname, join } from "node:path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+	return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    getAbsolutePath("@storybook/addon-onboarding"),
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@chromatic-com/storybook"),
-    getAbsolutePath("@storybook/addon-interactions"),
-    getAbsolutePath("@storybook/addon-a11y"),
-  ],
-  core: {
-    builder: '@storybook/builder-vite',
-  },
-  framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
-    options: {},
-  },
-  docs: {
-    autodocs: true,
-  },
-  async viteFinal(config, options) {
-    const { mergeConfig } = await import('vite')
+	stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+	addons: [
+		getAbsolutePath("@storybook/addon-onboarding"),
+		getAbsolutePath("@storybook/addon-links"),
+		getAbsolutePath("@storybook/addon-essentials"),
+		getAbsolutePath("@chromatic-com/storybook"),
+		getAbsolutePath("@storybook/addon-interactions"),
+		getAbsolutePath("@storybook/addon-a11y"),
+	],
+	core: {
+		builder: "@storybook/builder-vite",
+	},
+	framework: {
+		name: getAbsolutePath("@storybook/react-vite"),
+		options: {},
+	},
+	docs: {
+		autodocs: true,
+	},
+	async viteFinal(config, options) {
+		const { mergeConfig } = await import("vite");
 
-    if (options.configType === 'PRODUCTION') {
-      config.base = '/design-system/'
-    }
+		if (options.configType === "PRODUCTION") {
+			config.base = "/design-system/";
+		}
 
-    return mergeConfig(config, {})
-  }
+		return mergeConfig(config, {
+			build: {
+				minify: false,
+			},
+		});
+	},
 };
 export default config;
